54afa298f69828aadfc5156a1ffb9e277559690d
ojasvsingh <123519056+ojasvsingh@users.noreply.github.com>
2024-03-16 16:21:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+randomNum1 = Math.floor(Math.random() *6) +1;
+diceNum = "./images/dice" + randomNum1 + ".png";
+document.querySelector(".img1").setAttribute("src", diceNum);
+randomNum2 = Math.floor(Math.random() *6) +1;
+diceNum2 = "./images/dice" + randomNum2 + ".png";
+document.querySelector(".img2").setAttribute("src" , diceNum2);
+
+if(randomNum1>randomNum2){
+    document.querySelector("h1").textContent = "Player 1 Wins";
+}
+else if (randomNum1 < randomNum2){
+    document.querySelector("h1").textContent = "Player 2 Wins";
+}
+else{
+    document.querySelector("h1").textContent = "Draw";
+}
